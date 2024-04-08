a473ccfe6121a90c0f3c53a2ea7e752ba1aedea8
choimeeyoung <choimeeyoung94@gmail.com>
2024-04-08 12:20:25 +0900

    python 기초문법 실습 - 변수선언, 배열, 클래스, 함수, 조건문, 연산자

---
+++ main.py
@@ -0,0 +1,35 @@
+a = 2
+b = 3
+c = 5
+맥북 = '비싸'
+에어팟맥스 = '사고싶어'
+print(맥북)
+if a > b:
+    print('true')
+else:
+    print('false')
+
+array = [1,2,3,4,5]
+print(array[0])
+
+class Singer:
+    def 장르():
+        print('재즈')
+    def 이름():
+        print('아무개')
+    def sayHello():
+        print('안녕하세요~~')
+Singer.이름()
+Singer.sayHello()
+
+if True & True:
+    print('참입니다!!')
+
+if 1 > 0:
+    print('true')
+
+for i in range(1,5):
+    print(i)
+
+for i in array:
+    print(i)
